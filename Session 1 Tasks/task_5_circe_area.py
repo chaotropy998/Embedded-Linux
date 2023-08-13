a79d46e28809c0a955fa4d376d094ed875188180
Omar Mohamed <141978302+chaotropy998@users.noreply.github.com>
2023-08-13 19:20:40 +0300

    Create task_5_circe_area.py

---
+++ Session 1 Tasks/task_5_circe_area.py
@@ -0,0 +1,9 @@
+#Write a python script that calculates the area of a circle using its radius
+
+import math
+
+radius = float(input("Enter the circle radius: "))
+
+area = math.pi * radius ** 2
+
+print("The area of the circle is:", area)
