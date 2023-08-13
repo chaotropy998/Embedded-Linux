ad26605a34ebc70faef4bf1d02a5cc0e2f6ae2c9
Omar Mohamed <141978302+chaotropy998@users.noreply.github.com>
2023-08-13 19:17:51 +0300

    Create task_4_environment_variables.py

---
+++ Session 1 Tasks/task_4_environment_variables.py
@@ -0,0 +1,12 @@
+#Write a python code to access environment variables
+
+import os
+
+print("----------------------------------------")
+print(os.environ['PATH'])
+
+#To see list of all environment variables
+print("----------------------------------------")
+print("This is a list of all environment variables")
+print("----------------------------------------")
+print(os.environ)
