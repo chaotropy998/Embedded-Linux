592cac690cad493c16369aebfadbaf9f16779d73
Omar Mohamed <141978302+chaotropy998@users.noreply.github.com>
2023-08-13 17:27:16 +0300

    Update task_1_handle_login_system.py

--- Session 1 Tasks/task_1_handle_login_system.py
+++ Session 1 Tasks/task_1_handle_login_system.py
@@ -1,1 +1,14 @@
+users = {'Ahmed': '1394',
+        'Ali': '6078',
+        'Amr': '9345'}
 
+def login():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    if username in users and users[username] == password:
+        print("Welcome, {}!" .format(username))
+    else:
+        print("Incorrect Entry.")
+
+login()
