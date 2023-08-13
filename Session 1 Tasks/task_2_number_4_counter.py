168f005b9e6cdd50ec0d104df7b106842c909e06
Omar Mohamed <141978302+chaotropy998@users.noreply.github.com>
2023-08-13 18:38:01 +0300

    Update task_2_number_4_counter.py

--- Session 1 Tasks/task_2_number_4_counter.py
+++ Session 1 Tasks/task_2_number_4_counter.py
@@ -1,7 +1,7 @@
 def count_number(lst, num):
     return lst.count(num)
 
-lst = [1, 4, 3, 4, 5, 6, 4, 8, 9, 4, 10, 4, 4]
+lst = [1, 4, 3, 4, 4, 6, 4, 8, 9, 4, 10, 4, 4]
 num = 4
 
 print(f"Number {num} appeared {count_number(lst, num)} times in the given list.")
