e30a768ac575a9ae18d2c61d3284405fb83774eb
Omar Mohamed <141978302+chaotropy998@users.noreply.github.com>
2023-08-29 03:53:58 +0300

    Updated clipboard.py

--- General Tasks/clipboard.py
+++ General Tasks/clipboard.py
@@ -1,7 +1,23 @@
 import pyperclip as pc
-import pyautogui 
+import keyboard
+import os
 
-#pyautogui.hotkey('ctrl', 'c')
-#pc.copy
-pyautogui.alert("")
+file_path = "notes.txt"
 
+def export_clipboard_data():
+    clipboard_data = pc.paste()
+
+    # Create the notes.txt file if it doesn't exist
+    if not os.path.exists(file_path):
+        with open(file_path, "w"):
+            pass
+
+    # Append clipboard_data to the notes.txt file
+    with open(file_path, "a+") as file:
+        file.write(clipboard_data + "\n")
+    
+# Register the hotkey
+keyboard.add_hotkey("ctrl+alt+u", export_clipboard_data)
+
+# Keep the script running
+keyboard.wait()
